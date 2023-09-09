9a66157594c8b792dd00e768713688ab8a652517
Weizhi Song <songwz03@gmail.com>
2023-09-09 14:26:44 +0800

    -

--- build/lib/TreeSAK/PlotMcmcNode.py
+++ build/lib/TreeSAK/PlotMcmcNode.py
@@ -7,11 +7,11 @@ import plotly.express as px
 
 
 PlotMcmcNode_usage = '''
-========================= PlotMcmcNode example commands =========================
+============================ PlotMcmcNode example commands ============================
 
-TreeSAK PlotMcmcNode -i Clock2.txt -n n179 -o Clock2_n179.pdf
-TreeSAK PlotMcmcNode -i Clock2.txt -n n161,n186 -o Clock2_n161_n186.pdf
-TreeSAK PlotMcmcNode -i MCMC_op -n nodes.txt -o multi_runs_multi_nodes.pdf
+TreeSAK PlotMcmcNode -i McmcTree_op_files -n n179 -o Clock2_n179.pdf
+TreeSAK PlotMcmcNode -i McmcTree_op_files -n n161,n186 -o Clock3_n161_n186.pdf
+TreeSAK PlotMcmcNode -i McmcTree_op_files -n nodes.txt -o multi_runs_multi_nodes.pdf
 
 # File name of the mcmc.txt and the corresponding mcmc out file need to follow 
 # the rule as specified below:
@@ -24,11 +24,11 @@ node1	Bacteria
 node2
 node3,node9	Archaea
 
-# file format (-label, tab separated)
+# Y-axis label file format (-l, tab separated)
 PA_75_DeltaLL_50_clock3_mcmc.txt	DeltaLL_50
 PA_75_DeltaLL_75_clock3_mcmc.txt	DeltaLL_75
 
-=================================================================================
+=======================================================================================
 '''
 
 def sep_path_basename_ext(file_in):
@@ -46,7 +46,7 @@ def sep_path_basename_ext(file_in):
 
 def mcmctree_out_to_tree_str(mamctree_out):
 
-    # get tree string from mamctree_out
+    # get tree string from mcmctree_out
     tree_str = ''
     tree_line = 0
     current_line = 1
@@ -87,7 +87,7 @@ def plot_distribution(df_txt, output_plot):
     if plot_height < 360:
         plot_height = 360
 
-    fig = px.violin(df, x="Distribution", y="Setting", color="Node", points=False, orientation="h", width=plot_width, height=plot_height)
+    fig = px.violin(df, x="Value", y="Setting", color="Node", points=False, orientation="h", width=plot_width, height=plot_height)
     if len(node_id_list) == 1:
         fig.update_traces(side="positive", fillcolor='lightblue', width=1.6, opacity=0.75)
     else:
@@ -123,7 +123,7 @@ def get_internal_node_to_plot(node_txt, mo_file):
             else:
                 leaf_list = node_str.split(',')
                 if tree_str == '':
-                    print('MCMCTree out file not found, program exited!')
+                    print('*out.txt file not found, program exited!')
                     exit()
                 current_lca = Tree(tree_str, format=1).get_common_ancestor(leaf_list)
                 internal_node_to_plot = current_lca.name
@@ -147,7 +147,7 @@ def PlotMcmcNode(args):
     mcmc_in     = args['i']
     node_txt    = args['n']
     output_plot = args['o']
-    y_label_txt = args['label']
+    y_label_txt = args['l']
 
     # check MCMCTree output file/dir
     if os.path.isfile(mcmc_in) is True:
@@ -157,7 +157,17 @@ def PlotMcmcNode(args):
         mcmc_file_list = glob.glob(mcmc_file_re)
 
     if len(mcmc_file_list) == 0:
-        print('MCMCTree output file not found, program exited!')
+        print('*mcmc.txt file not found, program exited!')
+        exit()
+
+    missed_out_file_list = []
+    for each_mcmc_file in mcmc_file_list:
+        pwd_out_file = each_mcmc_file.replace('_mcmc.txt', '_out.txt')
+        if os.path.isfile(pwd_out_file) is False:
+            missed_out_file_list.append(pwd_out_file)
+    if len(missed_out_file_list) > 0:
+        print('The following *out.txt files are missing, program exited!')
+        print('\n'.join(sorted(missed_out_file_list)))
         exit()
 
     # read in y-axis label file
@@ -171,10 +181,11 @@ def PlotMcmcNode(args):
                 print('Format error: %s' % y_label_txt)
                 exit()
 
+    _, f_base, _ = sep_path_basename_ext(output_plot)
     found_matched_node = False
-    op_df_tmp    = '%s.txt'       % output_plot
-    op_label_tmp = '%s.label.txt' % output_plot
-    op_tree_tmp  = '%s.tree.txt'  % output_plot
+    op_tree_tmp  = '%s_tree.txt'  % f_base
+    op_df_tmp    = '%s_data.txt'  % f_base
+    op_label_tmp = '%s_label.txt' % f_base
 
     op_label_tmp_handle = open(op_label_tmp, 'w')
     op_tree_tmp_handle = open(op_tree_tmp, 'w')
@@ -208,6 +219,7 @@ def PlotMcmcNode(args):
         print('Provided node(s) not found, program exited!')
         exit()
 
+    # plot distribution
     plot_distribution(op_df_tmp, output_plot)
 
     print('Plot exported to %s, done!' % output_plot)
@@ -215,9 +227,9 @@ def PlotMcmcNode(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i',      required=True,                   help='mcmc.txt file or folder')
+    parser.add_argument('-i',      required=True,                   help='folder holds the *mcmc.txt and *out.txt files')
     parser.add_argument('-n',      required=True,                   help='Nodes to plot')
-    parser.add_argument('-label',  required=False, default=None,    help='labels on y axis')
+    parser.add_argument('-l',      required=False, default=None,    help='labels on y axis')
     parser.add_argument('-o',      required=True,                   help='Output plot')
     args = vars(parser.parse_args())
     PlotMcmcNode(args)
